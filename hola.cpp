44bd9796c0cccc81ad0a62ab98bf1a01034ab644
kimberlybarra <164095918+kimberlybarra@users.noreply.github.com>
2024-07-17 00:02:15 -0500

    suma de dos numeros

--- hola.cpp
+++ hola.cpp
@@ -6,5 +6,13 @@ int main(){
     cout<<"ingrese su nombre:  ";
     cin>>nombre;
     cout<<"hola mundo "<< nombre<<endl;
+    cout<<"mostraremos la suma de dos numero basico"<<endl;
+    int suma, a,b;
+    cout<<"ingrese 1er numero:  ";
+    cin>>a;
+    cout<<"ingrese segundo numero:  ";
+    cin>>b;
+    suma = a + b;
+    cout<<"la suma es:  "<<suma;
     return 0;
 }
